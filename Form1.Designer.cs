a9b396889f5a9528a328dc66a0808121d45b78c8
BenMarlatt <marlattbp@opstechalliance.com>
2018-10-18 22:44:22 -0400

    Init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Dynamic_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExploreButton = new System.Windows.Forms.Button();
+            this.ChangeDesktopButton = new System.Windows.Forms.Button();
+            this.DirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.DefaultDesktopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ExploreButton
+            // 
+            this.ExploreButton.Location = new System.Drawing.Point(12, 15);
+            this.ExploreButton.Name = "ExploreButton";
+            this.ExploreButton.Size = new System.Drawing.Size(75, 20);
+            this.ExploreButton.TabIndex = 0;
+            this.ExploreButton.Text = "Explore";
+            this.ExploreButton.UseVisualStyleBackColor = true;
+            this.ExploreButton.Click += new System.EventHandler(this.ExploreButton_Click);
+            // 
+            // ChangeDesktopButton
+            // 
+            this.ChangeDesktopButton.Location = new System.Drawing.Point(299, 15);
+            this.ChangeDesktopButton.Name = "ChangeDesktopButton";
+            this.ChangeDesktopButton.Size = new System.Drawing.Size(108, 20);
+            this.ChangeDesktopButton.TabIndex = 1;
+            this.ChangeDesktopButton.Text = "Change Desktop";
+            this.ChangeDesktopButton.UseVisualStyleBackColor = true;
+            this.ChangeDesktopButton.Click += new System.EventHandler(this.ChangeDesktopButton_Click);
+            // 
+            // DirectoryTextBox
+            // 
+            this.DirectoryTextBox.Location = new System.Drawing.Point(93, 15);
+            this.DirectoryTextBox.Name = "DirectoryTextBox";
+            this.DirectoryTextBox.Size = new System.Drawing.Size(200, 20);
+            this.DirectoryTextBox.TabIndex = 3;
+            // 
+            // DefaultDesktopButton
+            // 
+            this.DefaultDesktopButton.Location = new System.Drawing.Point(299, 41);
+            this.DefaultDesktopButton.Name = "DefaultDesktopButton";
+            this.DefaultDesktopButton.Size = new System.Drawing.Size(108, 20);
+            this.DefaultDesktopButton.TabIndex = 4;
+            this.DefaultDesktopButton.Text = "Default Desktop";
+            this.DefaultDesktopButton.UseVisualStyleBackColor = true;
+            this.DefaultDesktopButton.Click += new System.EventHandler(this.DefaultDesktopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 70);
+            this.Controls.Add(this.DefaultDesktopButton);
+            this.Controls.Add(this.DirectoryTextBox);
+            this.Controls.Add(this.ChangeDesktopButton);
+            this.Controls.Add(this.ExploreButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExploreButton;
+        private System.Windows.Forms.Button ChangeDesktopButton;
+        private System.Windows.Forms.TextBox DirectoryTextBox;
+        private System.Windows.Forms.Button DefaultDesktopButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dynamic_Desktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExploreButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.ShowDialog();
+            string folder = folderBrowser.SelectedPath;
+            if (folder == "")
+            {
+                MessageBox.Show("User Canceled");
+            }
+            else
+            {
+                //previousFilesToolStripMenuItem.
+                DirectoryTextBox.Text = folder;
+            }
+        }
+
+        private void ChangeDesktopButton_Click(object sender, EventArgs e)
+        {
+            ChangeDesktop.Change_Desktop(DirectoryTextBox.Text);
+        }
+
+        private void DefaultDesktopButton_Click(object sender, EventArgs e)
+        {
+            ChangeDesktop.Change_Desktop(@"C:\Users\" + Environment.UserName + @"\Desktop\");
+        }
+
+        private void checkPublicDesktop()
+        {
+            string PublicDesktopDir = @"C:\Users\Public\Desktop";
+            if (Directory.GetFiles(PublicDesktopDir).Length != 0)
+            {
+                DialogResult userAnswer = MessageBox.Show("The directory C:\\Users\\Public\\Desktop\\ contains files.\nWould you like to move them to your user's Desktop instead?\nIt's recommended to move them or files in the Public Desktop will always show.", "Do you want to move files?", MessageBoxButtons.YesNo);
+                if (userAnswer == DialogResult.Yes)
+                {
+
+                    string userDesktopDir = @"C:\Users\" + Environment.UserName + @"\Desktop\";
+                    DirectoryInfo pubdir = new DirectoryInfo(PublicDesktopDir);
+                    FileInfo[] files = pubdir.GetFiles();
+
+                    foreach (FileInfo file in files)
+                    {
+                        if (file.Length > 0)
+                        {
+                            if (File.Exists(userDesktopDir + file.Name))
+                            {
+                                File.Delete(userDesktopDir + file.Name);
+                            }
+                            file.MoveTo(userDesktopDir + file.Name);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
